bea24cce5a644f0a922a33b3478ab9556ba47a28
rebbe <MoppiMedda123>
2019-12-21 12:52:21 +0100

    primo commit con bozza di varie funzionalità
    -Swing Timer per il conto alla rovescia
    -Come scegliamo le frasi?
    -Come facciamo la ruota in sè?

---
+++ labBClient/src/PlayerRdF.java
@@ -0,0 +1,21 @@
+public class PlayerRdF  implements User {
+    private String name;
+    private String surname;
+    private String password;
+    private String emailAddress;
+    private String nickName;
+    private String userType; //observer/player
+
+    public void playGame(){
+
+    }
+
+    public void getDBInfo(){
+
+    }
+
+    public void observeGame(){
+
+    }
+
+}
